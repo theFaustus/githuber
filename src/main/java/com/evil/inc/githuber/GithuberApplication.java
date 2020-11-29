ec53f0b3f0d911a0c62d07976afc66706f49361b
Ion Pascari <ipascari@inthergroup.com>
2020-11-29 16:02:52 +0200

    add the project skeleton

---
+++ src/main/java/com/evil/inc/githuber/GithuberApplication.java
@@ -0,0 +1,13 @@
+package com.evil.inc.githuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithuberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evil/inc/githuber/GithuberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evil.inc.githuber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithuberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
